f719d69df72f924eca7572782cdb228eaf92b478
Anders Jurisoo <jurisoo@hotmail.com>
2018-12-05 10:04:46 +0800

    Added and comented simple example

--- src/main.py
+++ src/main.py
@@ -1,3 +1,46 @@
-import numpy
+# -*- coding: utf-8 -*-
 
-print("hej")
+import torch
+print(torch.__version__)
+
+dtype = torch.float
+device = torch.device("cpu")
+# device = torch.device("cuda:0") # Uncomment this to run on GPU
+
+# N is batch size; D_in is input dimension;
+# H is hidden dimension; D_out is output dimension.
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+# Create random input and output data
+x = torch.randn(N, D_in, device=device, dtype=dtype)
+
+print(x)
+
+y = torch.randn(N, D_out, device=device, dtype=dtype)
+
+# Randomly initialize weights
+w1 = torch.randn(D_in, H, device=device, dtype=dtype) # notice the H, seems we are working across all hidden layers
+w2 = torch.randn(H, D_out, device=device, dtype=dtype)
+
+learning_rate = 1e-6
+for t in range(500):
+    # Forward pass: compute predicted y
+    h = x.mm(w1) # <-- Matrix Multiplication x(input values * w1)
+    h_relu = h.clamp(min=0) # Set minimum value to 0
+    y_pred = h_relu.mm(w2) # 
+
+    # Compute and print loss
+    loss = (y_pred - y).pow(2).sum().item() # total loss across all 
+    print(t, loss)
+
+    # Backprop to compute gradients of w1 and w2 with respect to loss
+    grad_y_pred = 2.0 * (y_pred - y) # get the slope from the y difference (positive or negative times two)
+    grad_w2 = h_relu.t().mm(grad_y_pred) # t() transposes dimensions 0 and 1
+    grad_h_relu = grad_y_pred.mm(w2.t())
+    grad_h = grad_h_relu.clone()
+    grad_h[h < 0] = 0
+    grad_w1 = x.t().mm(grad_h)
+
+    # Update weights using gradient descent
+    w1 -= learning_rate * grad_w1
+    w2 -= learning_rate * grad_w2
