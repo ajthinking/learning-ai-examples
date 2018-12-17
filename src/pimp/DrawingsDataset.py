96273694718bca85c956990d79bd47feaccd8f50
Anders Jurisoo <jurisoo@hotmail.com>
2018-12-17 09:44:59 +0800

    Transformation of drawings

--- src/pimp/DrawingsDataset.py
+++ src/pimp/DrawingsDataset.py
@@ -14,40 +14,26 @@ class DrawingsDataset(data.Dataset):
     def __init__(self, root, train=True, transform=None, target_transform=None, download=False):
 
         with open('data/drawings.json') as file:
-            data = json.load(file)
+            drawings = json.load(file)
+            word_bins = self.get_word_bins(drawings)
+            for drawing in drawings:
+                pass
 
-        print(data[0]['name'])
+        print(word_bins)
+
+    def get_word_bins(self, drawings):
+        word_bins = []
+
+        for drawing in drawings:
+            for word in drawing['name'].split():
+                if not (
+                    word == '-' or
+                    word.isdigit() or
+                    word.lower() in word_bins
+                ):
+                    word_bins.append(word.lower())
+        
+        return word_bins
+
+    def __getitem__(self, something):
         return
-
-        self.root = os.path.expanduser(root)
-        self.transform = transform
-        self.target_transform = target_transform
-        self.train = train  # training set or test set
-
-        if download:
-            self.download()
-
-        if not self._check_exists():
-            raise RuntimeError('Dataset not found.' +
-                               ' You can use download=True to download it')
-
-        if self.train:
-            data_file = self.training_file
-        else:
-            data_file = self.test_file
-        self.data, self.targets = torch.load(os.path.join(self.processed_folder, data_file))
-
-    def __getitem__(self, index):
-        img, target = self.data[index], int(self.targets[index])
-
-        # doing this so that it is consistent with all other datasets
-        # to return a PIL Image
-        img = Image.fromarray(img.numpy(), mode='L')
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return img, target
